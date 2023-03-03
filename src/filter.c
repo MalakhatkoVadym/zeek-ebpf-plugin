07df1f0b38ef027d9e82fcc7530501867879e516
MalakhatkoVadym <v.malakhatko@sirinsoftware.com>
2023-03-03 05:01:26 -0500

    Added example code for filrtering packets by port

--- src/filter.c
+++ src/filter.c
@@ -6,78 +6,76 @@
 #include <linux/in.h>
 #include <linux/tcp.h>
 #include <linux/udp.h>
-#include <bpf/bpf_helpers.h>
+#include "bpf_helpers.h"
+
+#define IP_MF 0x2000
+#define IP_OFFSET 0x1FFF
+
+#ifndef likely
+#define likely(x) __builtin_expect(!!(x), 1)
+#endif
+
+#ifndef unlikely
+#define unlikely(x) __builtin_expect(!!(x), 0)
+#endif
+
+#undef offsetof
+#ifdef __compiler_offsetof
+#define offsetof(TYPE, MEMBER) __compiler_offsetof(TYPE, MEMBER)
+#else
+#define offsetof(TYPE, MEMBER) ((unsigned long)&((TYPE *)0)->MEMBER)
+#endif
+
+struct flow
+{
+    union
+    {
+        __u32 ports;
+        __be16 port16[2];
+    };
+};
+
+static inline int ip_is_fragment(struct __sk_buff *ctx, __u64 nhoff)
+{
+    return load_half(ctx, nhoff + offsetof(struct iphdr, frag_off)) & (IP_MF | IP_OFFSET);
+}
+
+static inline __u64 parse_ip(struct __sk_buff *skb, __u64 nhoff, __u64 *ip_proto)
+{
+    __u64 verlen;
+    if (unlikely(ip_is_fragment(skb, nhoff)))
+        *ip_proto = 0;
+    else
+        *ip_proto = load_byte(skb, nhoff + offsetof(struct iphdr, protocol));
+
+    verlen = load_byte(skb, nhoff + 0 /*offsetof(struct iphdr, ihl)*/);
+    if (likely(verlen == 0x45))
+        nhoff += 20;
+    else
+        nhoff += (verlen & 0xF) << 2;
+
+    return nhoff;
+}
 
 SEC("socket1")
 int bpf_prog1(struct __sk_buff *skb)
 {
-	return skb->len;
-	//return SK_PASS;
-	//return SK_DROP;
-	
-	void *data = (void *)(long)skb->data;
-    // void *data_end = (void *)(long)skb->data_end;
+    // __u64 nhoff = ETH_HLEN;
+    // __u64 ip_proto;
+    // // FILTERED PORT
+    // __be16 filter_port = 514;
 
-    struct ethhdr *eth = data;
-
-    // __u32 proto = load_half(skb, 12);
-    // if (data + sizeof(*eth) > data_end) {
-    //      return 0;
-    // }
-
-    if (eth->h_proto != __constant_htons(ETH_P_IP))
-    {
-        return skb->len;
-    }
-
-    struct iphdr *ip = data + sizeof(*eth);
-    // if ((void *)ip + 1 > data_end) {
-    //     return 0;
-    // }
-
-    if (ip->protocol != IPPROTO_TCP && ip->protocol != IPPROTO_UDP)
-    {
-        return skb->len;
-    }
-
-    // if (ip->protocol == IPPROTO_TCP) {
-    //     struct tcphdr *tcp = data + sizeof(*eth) + sizeof(*ip);
-    //     // if ((void *)tcp + 1 > data_end) {
-    //     //      return 0;
-    //     //  }
-
-    //     __be16 src_port = tcp->source;
-    //     __be16 dst_port = tcp->dest;
-
-    //     if (dst_port == 80) {
-    //         return SK_PASS;
+    // __u64 proto = load_half(skb, 12);
+    // if (likely(proto == ETH_P_IP))
+    // {
+    //     struct flow flow;
+    //     nhoff = parse_ip(skb, nhoff, &ip_proto);
+    //     flow.ports = load_word(skb, nhoff);
+    //     //__u64 ports_e = load_word(skb, nhoff);
+    //     if (flow.port16[0] == filter_port || flow.port16[1] == filter_port)
+    //     {
+    //         return SK_DROP;
     //     }
-    // }
-    // 	// struct PortFilterSpec dst_filter = {dst_port, IPPROTO_TCP};
-    // 	// struct port_stats *dst_stats = bpf_map_lookup_elem(&port_filtering_map, &dst_filter);
-    //     // if (dst_stats) {
-    //     //     dst_stats->count++;
-    // 	// 	return 0;
-    //     // }
-
-    // 	// struct PortFilterSpec src_filter = {src_port, IPPROTO_TCP};
-    // 	// struct port_stats *src_stats = bpf_map_lookup_elem(&port_filtering_map, &src_filter);
-    //     // if (src_stats) {
-    //     //     src_stats->count++;
-    // 	// 	return 0;
-    //     // }
-
-    //     // Do something with src_port and dst_port for TCP packets
-    // } else if (ip->protocol == IPPROTO_UDP) {
-    //     struct udphdr *udp = data + sizeof(*eth) + sizeof(*ip);
-    //     if ((void *)udp + 1 > data_end) {
-    //         return 0;
-    //     }
-
-    //     __be16 src_port = udp->source;
-    //     __be16 dst_port = udp->dest;
-
-    //     // Do something with src_port and dst_port for UDP packets
     // }
     return skb->len;
 }
